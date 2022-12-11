f8a8a6c2e193ddd1b862da65bd247a3354296952
Shaswat Prabhat <spd1238@gmail.com>
2022-12-11 16:31:24 +0530

    Add @astro/react package.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
