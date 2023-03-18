eb96c2f9653ae05ce518fde9d3ddec6e9b825a89
Shaswat Prabhat <spd1238@gmail.com>
2023-03-18 13:22:18 +0530

    Aligned footer using: https://moderncss.dev/keep-the-footer-at-the-bottom-flexbox-vs-grid/

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Shaswat Prabhat";
-export const SITE_DESCRIPTION = "Shswat's blog on tech and non-tech writings.";
+export const SITE_TITLE = "Shaswat's blog";
+export const SITE_DESCRIPTION = "Shswat's blog";
