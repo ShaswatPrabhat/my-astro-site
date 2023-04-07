c686774c0b1948c7775ca40c60d40ed3e08c244a
Shaswat Prabhat <spd1238@gmail.com>
2023-04-07 14:11:23 +0530

    Upgrade rss and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-
+import vercelServerless from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://example.com/",
   integrations: [mdx(), sitemap(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercelServerless(),
 });
