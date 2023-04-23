0d08b4078d658d009514999e6bf96051ee5e4cd9
Shaswat Prabhat <spd1238@gmail.com>
2023-04-23 10:15:59 +0530

    Add script to create new note

---
+++ src/scripts/create-note.mjs
@@ -0,0 +1,44 @@
+#!/usr/bin/env zx
+
+import { $, quiet, chalk } from "zx";
+
+const newFilePath = "src/pages/notes/new-note.mdx";
+const currentDate = new Date();
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dev",
+];
+
+let formattedDate = `${
+  months[currentDate.getMonth()]
+} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
+
+await quiet($`touch ${newFilePath}`);
+
+const frontMatter = `---
+layout: "../../layouts/BlogPost.astro"
+title: "New Note"
+description: "New Note created by script"
+pubDate: "${formattedDate}"
+heroImage: "/placeholder-hero.jpg"
+draft: true
+---`;
+
+await spinner("working...", () => $`sleep 2`);
+
+await quiet($`echo ${frontMatter} > ${newFilePath}`);
+
+console.log(
+  chalk.blue("New Note successfully created at ") +
+    chalk.red.bgBlue.bold(`${newFilePath}`)
+);
