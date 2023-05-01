e05a5b16bdb1f52f05768ddf255b18eaa6b18ac5
Shaswat Prabhat <spd1238@gmail.com>
2023-05-01 11:02:04 +0530

    Add content for new notes

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Shaswat's digital garden";
+export const SITE_TITLE_HINDI = "शाश्वत's digital garden";
 
 export const SITE_DESCRIPTION = "Shswat's digital garden";
