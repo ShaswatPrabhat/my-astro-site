c73241e3d6f7c6ca7b1d3a3cf1f5a098c3307b53
Shaswat Prabhat <spd1238@gmail.com>
2023-05-17 21:21:30 +0530

    Update create TIL script

--- src/scripts/create-til.mjs
+++ src/scripts/create-til.mjs
@@ -14,26 +14,27 @@ if (!fs.existsSync(path)) {
   console.log(chalk.red(`${dir} already exists`));
 }
 console.log(chalk.green(`Creating the new TIL file now`));
-const currentDate = new Date();
-let dd = currentDate.getDate();
-let mm = currentDate.getMonth();
-let hh = currentDate.getHours();
-let MM = currentDate.getMinutes();
-let ss = currentDate.getSeconds();
-let yyyy = currentDate.getFullYear();
-if (dd < 10) dd = "0" + dd;
-if (mm < 10) mm = "0" + mm;
-if (hh < 10) hh = "0" + hh;
-if (MM < 10) MM = "0" + MM;
-if (ss < 10) ss = "0" + ss;
-const formattedDate = `${dd}_${mm}_${yyyy}_${hh}_${MM}_${ss}`;
-const newFilePath = `${path}/${formattedDate}.mdx`;
 
+const tils = [];
+const directories = fs.readdirSync(`src/content/til/`);
+directories.forEach((directory) => {
+  const files = fs.readdirSync(`src/content/til/${directory}`);
+  tils.push(...files);
+});
+let max = 0;
+tils.forEach((til) => {
+  if (Number(til.split(".")[0]) > max) {
+    max = Number(til.split(".")[0]);
+  }
+});
+max += 1;
+const newFilePath = `${path}/${max}.mdx`;
 if (!fs.existsSync(newFilePath)) {
   console.log(chalk.red(`${newFilePath} does not exist`));
   console.log(chalk.green(`Creating ${newFilePath} now`));
   await spinner(chalk.green("Working..."), () => $`sleep 2`);
   await quiet($`touch ${newFilePath}`);
+  const currentDate = new Date();
   const months = [
     "Jan",
     "Feb",
