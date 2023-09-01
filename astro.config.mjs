9abfdfcac2008a9c934ce6137d58e90000c757b0
Shaswat Prabhat <spd1238@gmail.com>
2023-09-01 11:21:45 +0530

    Upgrade deps and notes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercelServerless from "@astrojs/vercel/serverless";
-// import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
